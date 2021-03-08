365c2c28a579ce525a956f8882daa1ffe72aa266
chronolaw <chrono_cpp@me.com>
2021-03-08 13:29:27 +0800

    std.hpp

---
+++ common/std.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 by Chrono
+
+#ifndef _COMMON_STD_HPP
+#define _COMMON_STD_HPP
+
+#include <cassert>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+
+//never 'using namespace std;' in c++ header
+
+#endif  //_COMMON_STD_HPP
+
