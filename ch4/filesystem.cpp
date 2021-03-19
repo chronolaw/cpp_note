edda73f0a6ecfcfdf7e38fe6b73b4c1b8c255a23
chronolaw <chrono_cpp@me.com>
2021-03-19 21:15:00 +0800

    filesystem.cpp

---
+++ ch4/filesystem.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2021 by Chrono
+//
+// g++ filesystem.cpp -std=c++17 -lstdc++fs -o a.out;./a.out
+// g++ filesystem.cpp -std=c++20 -o a.out;./a.out
+
+#include <cassert>
+
+#include <iostream>
+
+#if __has_include(<filesystem>)
+#include <filesystem>
+#else
+#include <experimental/filesystem>
+#endif
+
+void case1()
+{
+}
+
+int main()
+{
+    case1();
+
+    using namespace std;
+
+    cout << "filesystem demo" << endl;
+}
