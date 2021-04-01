5fe4e3ebf0950a47c542959b5b8a6dfde4354ecf
chronolaw <chrono_cpp@me.com>
2021-04-01 19:00:18 +0800

    setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# chrono @ 2021-03
+# chrono @ 2021-04
 
 # apt-get
 #DEBIAN_FRONTEND=noninteractive apt-get update
@@ -46,62 +46,61 @@ GPERF_VERSION="2.9.1"
 #echo ${JSON_VERSION}
 #echo ${HOME}
 
-mkdir ~/github
+#mkdir ~/github
 cd ~/github
 
 # test
 #exit
 
 # json
-curl -fsL https://github.com/nlohmann/json/releases/download/v${JSON_VERSION}/json.hpp -o json.hpp
+#curl -fsL https://github.com/nlohmann/json/releases/download/v${JSON_VERSION}/json.hpp -o json.hpp
 ln -s ~/github/json.hpp ~/${REPO}/common/
 
 # spdlog && fmtlib
-curl -fsL https://github.com/gabime/spdlog/archive/v${SPDLOG_VERSION}.tar.gz -o spdlog.tgz
-tar xfz spdlog.tgz
-cd spdlog-${SPDLOG_VERSION}
-mkdir build &&  cd build
-cmake .. && make -j && make install && make clean
-cd ..
+#curl -fsL https://github.com/gabime/spdlog/archive/v${SPDLOG_VERSION}.tar.gz -o spdlog.tgz
+#tar xfz spdlog.tgz
+#cd spdlog-${SPDLOG_VERSION}
+#mkdir build &&  cd build
+#cmake .. && make -j && make install && make clean
+#cd ..
 
 # curl/cpr
-curl -fsL https://github.com/whoshuu/cpr/archive/${CPR_VERSION}.tar.gz -o cpr.tgz
-tar xfz cpr.tgz
-cd cpr-${CPR_VERSION}
-cmake . -DUSE_SYSTEM_CURL=ON -DBUILD_CPR_TESTS=OFF
-make && make install && make clean
-cd ..
+#curl -fsL https://github.com/whoshuu/cpr/archive/${CPR_VERSION}.tar.gz -o cpr.tgz
+#tar xfz cpr.tgz
+#cd cpr-${CPR_VERSION}
+#cmake . -DUSE_SYSTEM_CURL=ON -DBUILD_CPR_TESTS=OFF
+#make && make install && make clean
+#cd ..
 
 # cinatra
-git clone https://github.com/qicosmos/cinatra --depth=1
+#git clone https://github.com/qicosmos/cinatra --depth=1
 #git clone git@github.com:qicosmos/cinatra.git --depth=1
 ln -s ~/github/cinatra/include/cinatra ~/${REPO}/common/cinatra
 ln -s ~/github/cinatra/include/cinatra.hpp ~/${REPO}/common/
 
 # luajit
-curl -fsL https://github.com/openresty/luajit2/archive/v${LUAJIT_VERSION}.tar.gz -o luajit.tgz
-tar xfz luajit.tgz
-cd luajit2-${LUAJIT_VERSION}
-make && make install && make clean
-ln -s /usr/local/lib/libluajit-5.1.so.2 /lib/x86_64-linux-gnu/
-cd ..
+#curl -fsL https://github.com/openresty/luajit2/archive/v${LUAJIT_VERSION}.tar.gz -o luajit.tgz
+#tar xfz luajit.tgz
+#cd luajit2-${LUAJIT_VERSION}
+#make && make install && make clean
+#ln -s /usr/local/lib/libluajit-5.1.so.2 /lib/x86_64-linux-gnu/
+#cd ..
 
 # luabridge
-curl -fsL https://github.com/vinniefalco/LuaBridge/archive/${LUABRIDAGE_VERSION}.tar.gz -o LuaBridge.tgz
-tar xfz LuaBridge.tgz
+#curl -fsL https://github.com/vinniefalco/LuaBridge/archive/${LUABRIDAGE_VERSION}.tar.gz -o LuaBridge.tgz
+#tar xfz LuaBridge.tgz
 ln -s ~/github/LuaBridge-${LUABRIDAGE_VERSION}/Source/LuaBridge/  ~/${REPO}/common/
 
 # gperftools
-curl -fsL https://github.com/gperftools/gperftools/releases/download/gperftools-${GPERF_VERSION}/gperftools-${GPERF_VERSION}.tar.gz -o gperf.tgz
-tar xfz gperf.tgz
-ln -s ~/github/gperftools-${GPERF_VERSION}/pprof-symbolize /bin/pprof
+#curl -fsL https://github.com/gperftools/gperftools/releases/download/gperftools-${GPERF_VERSION}/gperftools-${GPERF_VERSION}.tar.gz -o gperf.tgz
+#tar xfz gperf.tgz
+#ln -s ~/github/gperftools-${GPERF_VERSION}/pprof-symbolize /bin/pprof
 
 # flame graph
-git clone https://github.com/brendangregg/FlameGraph.git --depth=1
+#git clone https://github.com/brendangregg/FlameGraph.git --depth=1
 #git clone git@github.com:brendangregg/FlameGraph.git --depth=1
-ln -s ~/github/FlameGraph/flamegraph.pl /usr/local/bin/flamegraph.pl
-#ln -s ~/github/FlameGraph/flamegraph.pl ~/${REPO}/section4/
+#ln -s ~/github/FlameGraph/flamegraph.pl /usr/local/bin/flamegraph.pl
 
 # clean
-rm *.tgz
+#rm *.tgz
 
