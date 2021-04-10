80cb94b0197f94138d84ab95fd7a51ad5cf8b9bd
chronolaw <chrono_cpp@me.com>
2021-04-10 10:57:19 +0800

    par_algo.cpp

--- ch4/par_algo.cpp
+++ ch4/par_algo.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) 2021 by Chrono
 //
-// g++ par_algo.cpp -std=c++17 -o a.out;./a.out
-// g++ par_algo.cpp -std=c++20 -o a.out;./a.out
-// g++ par_algo.cpp -std=c++17 -I../common -o a.out;./a.out
+// g++ par_algo.cpp -std=c++20 -o a.out;time ./a.out
 
 #include <cassert>
 
@@ -18,13 +16,34 @@
 #error "no <execution> header file"
 #endif
 
+#include <ranges>
 #include <algorithm>
 
 using namespace std;
 
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+auto print = [](const auto& x){
+    cout << x << ",";
+};
+
 void case1()
 {
+    minstd_rand rand;
+    vector<string> v(1000 * 10);
 
+    //cout << to_string(rand());
+    ranges::generate(v, [&](){
+        return to_string(rand());
+    });
+
+    //ranges::for_each(v | views::take(10), print);
+
+    std::sort(execution::par, begin(v), end(v));
+    //std::sort(begin(v), end(v));
+
+    //cout << endl;
 }
 
 
