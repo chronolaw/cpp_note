971c310a43fae5de1dbbf705a2af8b06a81a20f1
chronolaw <chrono_cpp@me.com>
2021-04-13 11:08:42 +0800

    boost.sh

--- base/boost.sh
+++ base/boost.sh
@@ -3,4 +3,15 @@
 VER_DOT='1.75.0'
 VER_DASH='1_75_0'
 
-wget https://dl.bintray.com/boostorg/release/${VER_DOT}/source/boost_${VER_DASH}.tar.gz
+#wget https://dl.bintray.com/boostorg/release/${VER_DOT}/source/boost_${VER_DASH}.tar.gz
+
+tar xfz boost_${VER_DASH}.tar.gz
+cd boost_${VER_DASH}
+
+
+./bootstrap.sh
+./b2 tools/bcp
+
+./dist/bin/bcp lexical_cast ..
+./dist/bin/bcp string_algo ..
+./dist/bin/bcp property_tree ..
