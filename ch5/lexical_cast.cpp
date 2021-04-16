05d5b1ca7361dfaec1998ac34a385d462741feb0
chronolaw <chrono_cpp@me.com>
2021-04-16 13:14:47 +0800

    string_algo.cpp

--- ch5/lexical_cast.cpp
+++ ch5/lexical_cast.cpp
@@ -6,6 +6,7 @@
 #include <cassert>
 
 #include <iostream>
+//#include <string>
 
 #include <boost/lexical_cast.hpp>
 
@@ -16,11 +17,17 @@ void case1()
 {
     auto n1 = lexical_cast<short>("42");
     auto n2 = lexical_cast<int>("100");
-    auto n3 = lexical_cast<long>("64");
+    auto n3 = lexical_cast<long>("64000", 2);
+
+    auto f1 = lexical_cast<float>("1.414");
+    auto f2 = lexical_cast<double>("2.718");
 
     assert(n1 == 42);
     assert(n2 == 100);
     assert(n3 == 64);
+
+    auto x = stol("99L");
+    assert(x == 99L);
 }
 
 void case2()
@@ -30,6 +37,8 @@ void case2()
 
     assert(s1 == "42");
     assert(s2 == "100");
+
+    assert(to_string(0x64) == "100");
 }
 
 int main()
---
+++ ch5/string_algo.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2021 by Chrono
+//
+// g++ string_algo.cpp -std=c++17 -o a.out;./a.out
+// g++ string_algo.cpp -std=c++20 -o a.out;./a.out
+
+#include <cassert>
+
+#include <iostream>
+//#include <string>
+
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+using namespace boost;
+
+void case1()
+{
+}
+
+void case2()
+{
+}
+
+int main()
+{
+    case1();
+    case2();
+
+    cout << "string_algo demo" << endl;
+}
