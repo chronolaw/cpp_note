65d3e5282ccfff94a885ca091bdf65e96e22b55d
chronolaw <chrono_cpp@me.com>
2021-04-17 11:51:10 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,23 @@
 #GCC=gcc:10
 #GCC=gcc:7
 
-GCC=chronolaw/cpp_note:gcc10
+#GCC=chronolaw/cpp_note:gcc10
 #GCC=chronolaw/cpp_note:gcc7
 
+GCC=gcc7
+
+if [ $# -ne 0 ]; then
+    #echo $1
+    GCC=$1
+fi
+
 #echo ${GCC}
+#exit
+
+IMAGE="chronolaw/cpp_note:${GCC}"
 
 #docker run -it --rm -v ${PWD}:/tmp:ro ${GCC}
-docker run -it --rm -v ${PWD}:/tmp ${GCC}
+#docker run -it --rm -v ${PWD}:/tmp ${GCC}
+
+docker run -it --rm -v ${PWD}:/tmp ${IMAGE}
 
