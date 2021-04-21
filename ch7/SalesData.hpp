2531489724d042f36a388942949b59bc9a0e2877
chronolaw <chrono_cpp@me.com>
2021-04-21 13:49:02 +0800

    ch7

---
+++ ch7/SalesData.hpp
@@ -0,0 +1,108 @@
+// Copyright (c) 2021 by Chrono
+
+#ifndef _SALES_DATA_HPP
+#define _SALES_DATA_HPP
+
+#include "cpplang.hpp"
+
+#include <msgpack.hpp>
+
+#if MSGPACK_VERSION_MAJOR < 2
+#   error "msgpack  is too old"
+#endif
+
+BEGIN_NAMESPACE(cpp_note)
+
+// demo oop in C++
+class SalesData final
+{
+public:
+    using this_type = SalesData;
+
+public:
+    using string_type       = std::string;
+    using string_view_type  = std::string_view;
+    using uint_type         = unsigned int;
+    using currency_type     = double;
+
+    STATIC_ASSERT(sizeof(uint_type) >= 4);
+    STATIC_ASSERT(sizeof(currency_type) >= 4);
+public:
+    SalesData(string_view_type id, uint_type s, currency_type r) noexcept
+        : m_id(id), m_sold(s), m_revenue(r)
+    {}
+
+    SalesData(string_view_type id) noexcept
+        : SalesData(id, 0, 0)
+    {}
+
+public:
+    SalesData() = default;
+   ~SalesData() = default;
+
+    SalesData(const this_type&) = default;
+    SalesData& operator=(const this_type&) = default;
+
+    SalesData(this_type&& s) = default;
+    SalesData& operator=(this_type&& s) = default;
+
+private:
+    string_type m_id        = "";
+    uint_type   m_sold      = 0;
+    uint_type   m_revenue   = 0;
+
+public:
+    MSGPACK_DEFINE(m_id, m_sold, m_revenue);
+
+    msgpack::sbuffer pack() const
+    {
+        msgpack::sbuffer sbuf;
+        msgpack::pack(sbuf, *this);
+
+        return sbuf;
+    }
+
+    SalesData(const msgpack::sbuffer& sbuf)
+    {
+        auto obj = msgpack::unpack(
+                    sbuf.data(), sbuf.size()).get();
+        obj.convert(*this);
+    }
+
+public:
+    void inc_sold(uint_type s) noexcept
+    {
+        m_sold += s;
+    }
+
+    void inc_revenue(currency_type r) noexcept
+    {
+        m_revenue += r;
+    }
+public:
+    string_view_type id() const noexcept
+    {
+        return m_id;
+    }
+
+    uint_type sold() const noexcept
+    {
+        return m_sold;
+    }
+
+    currency_type revenue() const noexcept
+    {
+        return m_revenue;
+    }
+
+    CPP_DEPRECATED
+    currency_type average() const
+    {
+        return m_revenue / m_sold;
+    }
+};
+
+END_NAMESPACE(cpp_note)
+
+#endif  //_SALES_DATA_HPP
+
---
+++ ch7/Summary.hpp
@@ -0,0 +1,92 @@
+// Copyright (c) 2021 by Chrono
+
+#ifndef _SUMMARY_HPP
+#define _SUMMARY_HPP
+
+#include "cpplang.hpp"
+
+#if __cplusplus > 201703L
+#include <ranges>
+#endif
+
+#include "SalesData.hpp"
+#include "SpinLock.hpp"
+
+BEGIN_NAMESPACE(cpp_note)
+
+class Summary final
+{
+public:
+    using this_type = Summary;
+public:
+    using sales_type        = SalesData;
+    using lock_type         = SpinLock;
+    using lock_guard_type   = SpinLockGuard;
+
+    using string_type       = std::string;
+    using map_type          =
+            std::map<string_type, sales_type>;
+            //std::unordered_map<string_type, sales_type>;
+    using minmax_sales_type =
+            std::pair<string_type, string_type>;
+public:
+    Summary() = default;
+   ~Summary() = default;
+
+    Summary(const this_type&) = delete;
+    Summary& operator=(const this_type&) = delete;
+private:
+    mutable lock_type   m_lock;
+    map_type            m_sales;
+public:
+    void add_sales(const sales_type& s)
+    {
+        lock_guard_type guard(m_lock);
+
+        const auto& id = s.id();
+
+        // not found
+        if (m_sales.find(id) == m_sales.end()) {
+            m_sales[id] = s;
+            return;
+        }
+
+        // found
+        // you could use iter to optimize it
+        m_sales[id].inc_sold(s.sold());
+        m_sales[id].inc_revenue(s.revenue());
+    }
+
+    minmax_sales_type minmax_sales() const
+    {
+        lock_guard_type guard(m_lock);
+
+        if (m_sales.empty()) {
+            return minmax_sales_type();
+        }
+
+        // algorithm
+#if __cplusplus <= 201703L
+        auto ret = std::minmax_element(
+            std::begin(m_sales), std::end(m_sales),
+            [](const auto& a, const auto& b)
+            {
+                return a.second.sold() < b.second.sold();
+            });
+#else
+        auto ret = std::ranges::minmax_element(
+            m_sales, std::less{}, &SalesData::sold);
+#endif
+
+        // min max
+        auto min_pos = std::get<0>(ret);   //ret.first;
+        auto max_pos = std::get<1>(ret);   //ret.second;
+
+        return {min_pos->second.id(), max_pos->second.id()};
+    }
+};
+
+END_NAMESPACE(cpp_note)
+
+#endif  //_SUMMARY_HPP
+
