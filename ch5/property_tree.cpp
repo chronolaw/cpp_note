bea1dd8626f97c467559ba6282d3fd51d8b8c277
chronolaw <chrono_cpp@me.com>
2021-04-25 15:56:14 +0800

    property_tree.cpp

---
+++ ch5/property_tree.cpp
@@ -0,0 +1,55 @@
+// Copyright (c) 2021 by Chrono
+//
+// g++ property_tree.cpp -std=c++17 -o a.out;./a.out
+// g++ property_tree.cpp -std=c++20 -o a.out;./a.out
+
+#include <cassert>
+
+#include <iostream>
+#include <sstream>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+using namespace std;
+using namespace boost::property_tree;
+
+inline namespace detail{
+
+auto conf_str = R"({
+    "enable" : true,
+    "env" : "test",
+    "timeout" : 10,
+    "secret" : {
+        "crt" : "ccc",
+        "key" : "kkk"
+    },
+    "uri" : ["a", "b", "c"]
+    })";
+
+stringstream conf_stream{conf_str};
+}
+
+void case1()
+{
+    ptree pt;
+
+    read_json(conf_stream, pt);
+
+    cout << pt.get<bool>("enable") << endl;
+    cout << pt.get<string>("env") << endl;
+    cout << pt.get<int>("timeout") << endl;
+
+    cout << pt.get<string>("secret.crt") << endl;
+    cout << pt.get<string>("secret.key") << endl;
+
+    for(auto& [i, x] : pt.get_child("uri")) {
+        cout << x.data() << endl;
+    }
+}
+
+int main()
+{
+    case1();
+
+    cout << "property_tree demo" << endl;
+}
