58ec2be1f01a79a643b259dbb08871a18a8c0ab5
chronolaw <chrono_cpp@me.com>
2021-04-26 21:25:34 +0800

    selfsign.sh

--- cert/selfsign.sh
+++ cert/selfsign.sh
@@ -8,9 +8,13 @@ if [ -z "$1" ]; then
 fi
 
 domain=$1
+
+#ca="${domain}"
 ca="${domain}.ca"
 
-openssl req -x509 -days 3650 -out ${domain}.crt -keyout ${domain}.key \
+days=3650
+
+openssl req -x509 -days ${days} -out ${domain}.crt -keyout ${domain}.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj "/CN=${ca}" -extensions EXT -config <( \
    printf "[dn]\nCN=${domain}\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:${domain}\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
