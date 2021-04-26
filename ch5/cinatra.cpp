423c802b35c84007a09fac03c0681aff149a60fd
chronolaw <chrono_cpp@me.com>
2021-04-26 19:01:52 +0800

    cinatra.cpp

--- ch5/cinatra.cpp
+++ ch5/cinatra.cpp
@@ -30,10 +30,6 @@ void srv1()
         cout << req.raw_url() << endl;
         cout << "header: \n" << req.head() << endl;
 
-        if (req.has_body()) {
-            cout << "body: \n" <<  req.body() << endl;
-        }
-
         res.set_status(status_type::ok);
         res.set_content("hello http srv\n");
         res.build_response_str();
@@ -46,10 +42,61 @@ void srv1()
     srv.run();
 }
 
+
+void srv2()
+{
+    const auto max_threads = 2;
+    const auto addr = "0.0.0.0"s;
+    const auto port = 80;
+
+    http_server srv(max_threads);
+
+    if (srv.port_in_use(port)) {
+        cout << "port_in_use" << endl;
+        return;
+    }
+
+    srv.listen(addr, to_string(port));
+
+    srv.set_http_handler<GET>(
+        "/",
+        [](auto& req, auto& res) {
+            res.set_status_and_content(
+                status_type::forbidden, "code:403");
+        });
+
+    srv.set_http_handler<GET, POST>(
+        "/api",
+        [](auto& req, auto& res) {
+            auto method = req.get_method();
+
+            if (method == GET) {
+                res.set_status_and_content(
+                        status_type::not_found, "code:404");
+                return;
+            }
+
+            assert(method == POST);
+
+            if (!req.has_body()) {
+                res.set_status_and_content(
+                        status_type::bad_request, "code:400");
+                return;
+            }
+
+            //cout << "body: \n" <<  req.body() << endl;
+            res.set_status_and_content(
+                    status_type::ok, "data:"s + req.body());
+        });
+
+    srv.run();
+}
+
 int main()
 {
     cout << "cinatra http srv demo" << endl;
 
     srv1();
+    //srv2();
 }
 
